cc2bb754bc51505987f547bea6b4bf5049cbd8cf
전일웅 <jhun@nm-4ones.com>
2018-10-25 13:52:02 +0900

    정신나간 데코페턴

--- src/test/java/com/glqdlt/ex/replication/HelloImplTest.java
+++ src/test/java/com/glqdlt/ex/replication/HelloImplTest.java
@@ -1,10 +1,14 @@
 package com.glqdlt.ex.replication;
 
+import com.glqdlt.ex.replication.HelloDeco.HelloDeco;
+import com.glqdlt.ex.replication.HelloDeco.HelloUppercase;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+@Slf4j
 public class HelloImplTest {
 
     @Test
@@ -14,5 +18,18 @@ public class HelloImplTest {
         Assert.assertEquals("Hello Jhun", hello.sayHello("Jhun"));
         Assert.assertEquals("Hi Jhun", hello.sayHi("Jhun"));
         Assert.assertEquals("THX Jhun", hello.sayThankYou("Jhun"));
+    }
+
+//    정신 나간 데코의 데코 데코데코 패턴
+    @Test
+    public void decoTest() {
+        HelloImpl hello = new HelloImpl();
+        HelloDeco helloDeco = new HelloDeco(hello);
+        HelloUppercase helloUppercase = new HelloUppercase(helloDeco);
+        HelloDeco helloDeco1 = new HelloDeco(helloUppercase);
+
+        log.info(helloDeco.sayHello("jhun"));
+        log.info(helloUppercase.sayHello("jhun"));
+        log.info(helloDeco1.sayHello("jhun"));
     }
 }
