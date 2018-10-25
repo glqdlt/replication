da09df4580cc5564e581301d82eaa95d0afc0dd3
전일웅 <jhun@nm-4ones.com>
2018-10-25 14:44:20 +0900

    조금 더 유연한 다이나믹 프록시

--- src/main/java/com/glqdlt/ex/replication/HelloDeco/DynamicHelloUpperCase.java
+++ src/main/java/com/glqdlt/ex/replication/HelloDeco/DynamicHelloUpperCase.java
@@ -8,16 +8,20 @@ import java.lang.reflect.Method;
 // 굳이 Hello 의 구현체를 소스상에서 선언해서 사용하지 않고, deco 할 로직만 작성 후 invoke () 메소드에 담아서 deco 처리하는 다이나믹프록시
 public class DynamicHelloUpperCase implements InvocationHandler {
 
-    private Hello hello;
+    private Object hello;
 
-    public DynamicHelloUpperCase(Hello hello) {
+    public DynamicHelloUpperCase(Object hello) {
         this.hello = hello;
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        String ret = (String) method.invoke(hello,args);
+        Object ret = method.invoke(hello, args);
+        if (ret instanceof String) {
+            return ((String) ret).toUpperCase();
+        } else {
+            return ret;
+        }
 
-        return ret.toUpperCase();
     }
 }
